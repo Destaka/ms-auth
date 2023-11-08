da56d27246e27e7759dc5a55266104eb27efb10c
Dijango Alves <dijango_ilha@hotmail.com>
2023-11-07 22:01:20 -0300

    feat: add binds

--- src/framework/ioc/operatorModule.ts
+++ src/framework/ioc/operatorModule.ts
@@ -1,7 +1,11 @@
 import { ContainerModule, interfaces } from 'inversify'
 
 import { CreateUserIdentityOperator } from '../../controller/operators/users/createUserIdentityOperator'
+import { AuthorizerOperator } from '../../controller/operators/authorizer/authorizerOperator'
+import { SignInOperator } from '../../controller/operators/users/signInOperator'
 
 export const OperatorModule = new ContainerModule((bind: interfaces.Bind) => {
   bind(CreateUserIdentityOperator).toSelf()
+  bind(AuthorizerOperator).toSelf()
+  bind(SignInOperator).toSelf()
 })
--- src/framework/ioc/useCasesModule.ts
+++ src/framework/ioc/useCasesModule.ts
@@ -1,8 +1,14 @@
 import { ContainerModule, interfaces } from 'inversify'
 
 import { CreateUserIdentityUseCase } from '../../business/useCases/users/createUserIdentityUseCase'
+import { SignInUseCase } from '../../business/useCases/users/signInUseCase'
+import { GeneratePolicyUserCase } from '../../business/useCases/authorizer/generatePolicyUseCase'
+import { AuthorizerUseCase } from '../../business/useCases/authorizer/authorizerUseCase'
 
 
 export const UseCasesModule = new ContainerModule((bind: interfaces.Bind) => {
   bind(CreateUserIdentityUseCase).toSelf()
+  bind(SignInUseCase).toSelf()
+  bind(GeneratePolicyUserCase).toSelf()
+  bind(AuthorizerUseCase).toSelf()
 })
